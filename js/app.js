b3e72e7aeaa912634fab1890618c8a308a892f04
Jacob Pacheco <72306809+JacobPacheco100@users.noreply.github.com>
2022-07-11 18:23:14 -0400

    shopping cart layout complete

--- js/app.js
+++ js/app.js
@@ -1,25 +1,33 @@
 // quantity box
 
-const quantity = document.querySelector(".quantity")
-const item_quantity = document.querySelector(".quant")
+// const quantity = document.querySelector(".quantity")
+// const item_quantity = document.querySelector(".quant")
 
-let edit_quantity = 1
-item_quantity.textContent = edit_quantity
+// let edit_quantity = 1
+// item_quantity.textContent = edit_quantity
 
-quantity.addEventListener("click", (event) => {
-  const e = event.target.classList
+// quantity.addEventListener("click", (event) => {
+//   const e = event.target.classList
 
-  if (e.contains("add")) {
-    edit_quantity += 1
-    item_quantity.textContent = edit_quantity
-  } else if (e.contains("less")) {
-    if (edit_quantity <= 1) {
-      edit_quantity = 1
-    } else {
-      edit_quantity -= 1
-      item_quantity.textContent = edit_quantity
-    }
-  }
-})
+//   if (e.contains("add")) {
+//     edit_quantity += 1
+//     item_quantity.textContent = edit_quantity
+//   } else if (e.contains("less")) {
+//     if (edit_quantity <= 1) {
+//       edit_quantity = 1
+//     } else {
+//       edit_quantity -= 1
+//       item_quantity.textContent = edit_quantity
+//     }
+//   }
+// })
 
 // shopping cart
+
+// show cart
+const cart = document.querySelector(".shopping-cart")
+const show_cart = document.getElementById("cart-icon")
+
+show_cart.addEventListener("click", (e) => {
+  cart.classList.toggle("show-cart")
+})
