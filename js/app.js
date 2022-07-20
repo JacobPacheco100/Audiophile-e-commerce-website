f49430cbabd39acf9db1c2e1a91f65e984c96917
Jacob Pacheco <72306809+JacobPacheco100@users.noreply.github.com>
2022-07-20 13:15:06 -0400

    js/JSON : display data to category pages.. WIP || current obj: get data to display on each page (not just one)

--- js/app.js
+++ js/app.js
@@ -1,33 +1,46 @@
-// quantity box
+async function populate() {
+  const url = "/js/data.json"
+  const request = new Request(url)
 
-// const quantity = document.querySelector(".quantity")
-// const item_quantity = document.querySelector(".quant")
+  const response = await fetch(request)
+  const data = await response.json()
 
-// let edit_quantity = 1
-// item_quantity.textContent = edit_quantity
+  // headphones(data)
+  speakers(data)
+  // earphones(data)
+}
 
-// quantity.addEventListener("click", (event) => {
-//   const e = event.target.classList
+function headphones(data) {
+  const data1 = document.getElementById("data1")
+  const data2 = document.getElementById("data2")
+  const data3 = document.getElementById("data3")
 
-//   if (e.contains("add")) {
-//     edit_quantity += 1
-//     item_quantity.textContent = edit_quantity
-//   } else if (e.contains("less")) {
-//     if (edit_quantity <= 1) {
-//       edit_quantity = 1
-//     } else {
-//       edit_quantity -= 1
-//       item_quantity.textContent = edit_quantity
-//     }
-//   }
-// })
+  showProducts(data1, data, 1)
+  showProducts(data2, data, 2)
+  showProducts(data3, data, 3)
+}
 
-// shopping cart
+function speakers(data) {
+  const data4 = document.getElementById("data4")
+  const data5 = document.getElementById("data5")
 
-// show cart
-const cart = document.querySelector(".shopping-cart")
-const show_cart = document.getElementById("cart-icon")
+  showProducts(data4, data, 4)
+  showProducts(data5, data, 5)
+}
 
-show_cart.addEventListener("click", (e) => {
-  cart.classList.toggle("show-cart")
-})
+function earphones(data) {
+  const data0 = document.getElementById("data0")
+  showProducts(data0, data, 1)
+}
+
+function showProducts(card, data, object) {
+  const productName = card.querySelector("h2")
+  const productDescription = card.querySelector("p")
+
+  productName.textContent = data[object].name
+  productDescription.textContent = data[object].description
+}
+
+populate()
+
+// create a function that takes in array of objects (array containing relevent page products)
